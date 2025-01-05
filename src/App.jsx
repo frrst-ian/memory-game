c8101b7479cd59eaae502228e49ec1c4406f69b1
frrst-ian <frrstxd@proton.me>
2025-01-05 09:01:39 +0800

    feat: add the game score management

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from "react";
+import Scoreboard from './components/Scoreboard';
+import Gameboard from './components/Gameboard';
 
 function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
 
+  const handleGameOver = (finalScore) => {
+    if (finalScore > bestScore) {
+      setBestScore(finalScore);
+    }
+    setScore(0);
+  };
+
+  const handleScore = () => {
+    setScore(score + 1);
+  };
+
   return (
-    <>
-      <div>Scoreboard</div>
-      <div>Game board</div>
-    </>
+    <div className="container">
+        <h1>Memory Game</h1>
+        <Scoreboard score ={score} bestScore={bestScore}/>
+        <Gameboard onScore={handleScore} onGameOver={handleGameOver}/>
+    </div>
+    
   );
 }
 
