405a474d2d660d19dbb8569faf7f5154b6ff105c
frrst-ian <frrstxd@proton.me>
2025-01-05 08:32:20 +0800

    fix: remove the codes in Card.jsx and put it on Scoreboard.jsx

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,12 +1,2 @@
 /* eslint-disable react/prop-types */
 
-const Scoreboard = ({ score, bestScore }) => {
-  return (
-    <div className="container">
-      <div className="text">Current Score: {score}</div>
-      <div className="text">Best Score: {bestScore} </div>
-    </div>
-  );
-};
-
-export default Scoreboard;
---
+++ components/Scoreboard.jsx
@@ -0,0 +1,11 @@
+const Scoreboard = ({ score, bestScore }) => {
+    return (
+      <div className="container">
+        <div className="text">Current Score: {score}</div>
+        <div className="text">Best Score: {bestScore} </div>
+      </div>
+    );
+  };
+  
+  export default Scoreboard;
+  
