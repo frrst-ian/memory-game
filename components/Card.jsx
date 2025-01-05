7a1266405a3fc206ada5aab3a5126e25a7d1f94d
frrst-ian <frrstxd@proton.me>
2025-01-05 08:37:33 +0800

    feat: add card components that manage card information

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,2 +1,11 @@
 /* eslint-disable react/prop-types */
+const Card = ({ card, onClick }) => {
+  return (
+    <div onClick={onClick} className="cards">
+      <img src="card.image" alt="card.name" className="card" />
+      <h3 className="card-name">{card.name}</h3>
+    </div>
+  );
+};
 
+export default Card;
