f0e220fdb2e8c9602b5d4d5bb935d2e5e4a0986e
frrst-ian <frrstxd@proton.me>
2025-01-05 08:24:54 +0800

    feat: add the main structure in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
 
   return (
     <>
-      <p>Hello, World!</p>
+      <div>Scoreboard</div>
+      <div>Game board</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
